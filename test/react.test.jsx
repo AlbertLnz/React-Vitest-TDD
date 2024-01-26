984cd7950b1a32419d09f97c8feb0c5fa78b4ed6
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-26 14:17:19 +0100

    4th Test React - Should render from 0 to 9 numbers

--- test/react.test.jsx
+++ test/react.test.jsx
@@ -1,28 +1,43 @@
 import { render, screen, cleanup } from '@testing-library/react'
 import { afterEach, describe, it, expect } from 'vitest'
 
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 const Calculator = () => {
   return (
-    <h2>Calculator</h2>
+    <div>
+      <h2>Calculator</h2>
+      {numbers.map((number) => (
+        <span key={number}>{number}</span>
+      ))}
+    </div>
   )
 }
 
 describe('Calculator', () => {
   afterEach(cleanup)
 
-  it('Should render', () => {
+  it('Should render', () => { // Test nº1
     render(<Calculator />)
   })
 
-  it('Should render title correctly', () => {
+  it('Should render title correctly', () => { // Test nº2
     render(<Calculator />) // <-- check component if it's rendered
     screen.getByText('Calculator') // <-- check if component has title name called 'Calculator'
   })
 
-  it('Should render title correctly with <h2> tag', () => {
+  it('Should render title correctly with <h2> tag', () => { // Test nº3
     render(<Calculator />)
     const titleElement = screen.getByText('Calculator')
 
     expect(titleElement.tagName).toBe('H2')
+  })
+
+  it('Should render numbers [0-9]', () => {
+    render(<Calculator />)
+
+    numbers.forEach(number => {
+      screen.getByText(number)
+    })
   })
 })
