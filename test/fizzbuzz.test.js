250ef4d84215bfb807cc02d5b89516393d8e9415
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-26 12:20:48 +0100

    Refactor if's

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -4,11 +4,14 @@ const fizzbuzz = (number) => {
   if (typeof number !== 'number') throw new Error('Parameter provided must be a number!!')
   if (Number.isNaN(number)) throw new Error('Parameter provided must be a number!!')
 
-  if (number % 15 === 0) return 'fizzbuzz'
-  if (number % 3 === 0) return 'fizz'
-  if (number % 5 === 0) return 'buzz'
+  const multiples = { 3: 'fizz', 5: 'buzz' }
+  let output = ''
 
-  return number
+  Object.entries(multiples).forEach(([multiplier, word]) => {
+    if (number % multiplier === 0) output += word
+  })
+
+  return output === '' ? number : output
 }
 
 describe('fizzbuzz', () => {
