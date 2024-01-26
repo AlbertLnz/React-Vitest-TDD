da8963a57a9907dab251f11d4fa90987ebcee87f
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-26 12:17:35 +0100

    multiple of 3 & 5 === multiple of 15

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -4,7 +4,7 @@ const fizzbuzz = (number) => {
   if (typeof number !== 'number') throw new Error('Parameter provided must be a number!!')
   if (Number.isNaN(number)) throw new Error('Parameter provided must be a number!!')
 
-  if (number % 3 === 0 && number % 5 === 0) return 'fizzbuzz'
+  if (number % 15 === 0) return 'fizzbuzz'
   if (number % 3 === 0) return 'fizz'
   if (number % 5 === 0) return 'buzz'
 
