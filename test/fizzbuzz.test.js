70a9008fbec456a56571a19c15f207ab05febe6f
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-26 11:36:43 +0100

    3rd test - Specific error message

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,7 +1,7 @@
 import { describe, expect, it } from 'vitest'
 
 const fizzbuzz = (number) => {
-  if (typeof number !== 'number') throw new Error()
+  if (typeof number !== 'number') throw new Error('Parameter provided must be a number!!')
 }
 
 describe('fizzbuzz', () => {
@@ -11,5 +11,9 @@ describe('fizzbuzz', () => {
 
   it('Should throw if not number is provided as parameter', () => { // test nº2
     expect(() => fizzbuzz()).toThrow()
+  })
+
+  it('Should throw a specific error message if not number is provided as parameter', () => {
+    expect(() => fizzbuzz()).toThrow('Parameter provided must be a number!!')
   })
 })
