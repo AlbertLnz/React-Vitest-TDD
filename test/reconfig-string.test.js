0ca2f6ab05487245416a4a9ee9f6dcbb3b5e7434
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-26 12:47:39 +0100

    Test n2 - Throw if function reconfigString hasn't a param

--- test/reconfig-string.test.js
+++ test/reconfig-string.test.js
@@ -1,11 +1,15 @@
 import { describe, expect, it } from 'vitest'
 
-const reconfigString = () => {
-  
+const reconfigString = (from) => {
+  if (from === undefined) throw new Error("'from' param is required")
 }
 
 describe('reconfigString', () => {
-  it('Should be a function', () => {
+  it('Should be a function', () => { // 1st test
     expect(reconfigString).toBeTypeOf('function')
+  })
+
+  it('Should throw if first param is missing', () => { // 2nd test
+    expect(() => reconfigString()).toThrow()
   })
 })
