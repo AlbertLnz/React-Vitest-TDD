53d20fba88997a6a0d2ddbac6979a64d6610d47a
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-26 14:13:16 +0100

    3rd Test React - Title has to be a H2 tag

--- test/react.test.jsx
+++ test/react.test.jsx
@@ -1,9 +1,9 @@
 import { render, screen, cleanup } from '@testing-library/react'
-import { afterEach, describe, it } from 'vitest'
+import { afterEach, describe, it, expect } from 'vitest'
 
 const Calculator = () => {
   return (
-    <h1>Calculator</h1>
+    <h2>Calculator</h2>
   )
 }
 
@@ -17,5 +17,12 @@ describe('Calculator', () => {
   it('Should render title correctly', () => {
     render(<Calculator />) // <-- check component if it's rendered
     screen.getByText('Calculator') // <-- check if component has title name called 'Calculator'
+  })
+
+  it('Should render title correctly with <h2> tag', () => {
+    render(<Calculator />)
+    const titleElement = screen.getByText('Calculator')
+
+    expect(titleElement.tagName).toBe('H2')
   })
 })
