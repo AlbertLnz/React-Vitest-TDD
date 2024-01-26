e5f90fdbfb991d82c22ff3e88b9221f90789c197
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-26 14:28:37 +0100

    5th Test React - Calculator has to have 4 rows of numbers

--- test/react.test.jsx
+++ test/react.test.jsx
@@ -2,15 +2,25 @@ import { render, screen, cleanup } from '@testing-library/react'
 import { afterEach, describe, it, expect } from 'vitest'
 
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const rows = [
+  [7, 8, 9],
+  [4, 5, 6],
+  [1, 2, 3],
+  [0]
+]
 
 const Calculator = () => {
   return (
-    <div>
+    <section>
       <h2>Calculator</h2>
-      {numbers.map((number) => (
-        <span key={number}>{number}</span>
-      ))}
-    </div>
+      <div role='grid'>
+        {rows.map((row, index) => (
+          <div key={index} role='row'>
+            {row.map(number => <span key={number}>{number}</span>)}
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
@@ -33,11 +43,18 @@ describe('Calculator', () => {
     expect(titleElement.tagName).toBe('H2')
   })
 
-  it('Should render numbers [0-9]', () => {
+  it('Should render numbers [0-9]', () => { // Test nº4
     render(<Calculator />)
 
     numbers.forEach(number => {
       screen.getByText(number)
     })
+  })
+
+  it('Should render 4 rows', () => { // Test nº5
+    render(<Calculator />)
+
+    const rows = screen.getAllByRole('row')
+    expect(rows).toHaveLength(4)
   })
 })
