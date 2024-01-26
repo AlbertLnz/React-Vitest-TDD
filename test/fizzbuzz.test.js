d22407e5b983a3ee79a57806cc64cfde3a20a37a
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-26 11:43:02 +0100

    4th test - Should be a number param on fizzbuzz function

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -2,6 +2,7 @@ import { describe, expect, it } from 'vitest'
 
 const fizzbuzz = (number) => {
   if (typeof number !== 'number') throw new Error('Parameter provided must be a number!!')
+  if (Number.isNaN(number)) throw new Error('Parameter provided must be a number!!')
 }
 
 describe('fizzbuzz', () => {
@@ -13,7 +14,11 @@ describe('fizzbuzz', () => {
     expect(() => fizzbuzz()).toThrow()
   })
 
-  it('Should throw a specific error message if not number is provided as parameter', () => {
+  it('Should throw a specific error message if not number is provided as parameter', () => { // test nº3
     expect(() => fizzbuzz()).toThrow('Parameter provided must be a number!!')
+  })
+
+  it('Should be a number param', () => { // test nº4
+    expect(() => fizzbuzz(NaN)).toThrow('Parameter provided must be a number!!')
   })
 })
