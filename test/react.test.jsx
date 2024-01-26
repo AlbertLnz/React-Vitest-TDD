aeefc97edc64fe8b2ecb69f83d472db79c4e50ae
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-26 14:07:20 +0100

    Problem on 2nd Test React - 2 times component rendered (lines 12 & 16). Need to clean with cleanup

--- test/react.test.jsx
+++ test/react.test.jsx
@@ -1,12 +1,19 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { describe, it } from 'vitest'
 
 const Calculator = () => {
-  
+  return (
+    <h1>Calculator</h1>
+  )
 }
 
 describe('Calculator', () => {
   it('Should render', () => {
     render(<Calculator />)
+  })
+
+  it('Should render title correctly', () => {
+    render(<Calculator />) // <-- check component if it's rendered
+    screen.getByText('Calculator') // <-- check if component has title name called 'Calculator'
   })
 })
