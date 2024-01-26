9c14740187afe33d62446c59ab15dc94f00a1997
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-26 13:06:44 +0100

    Test n4 - Throw if 2nd param (to) is not a string

--- test/reconfig-string.test.js
+++ test/reconfig-string.test.js
@@ -1,20 +1,25 @@
 import { describe, expect, it } from 'vitest'
 
-const reconfigString = (from) => {
-  if (from === undefined) throw new Error("'from' param is required")
+const reconfigString = (from, to) => {
+  // if (from === undefined) throw new Error("'from' param is required")
   if (typeof from !== 'string') throw new Error("'from' param must be a string")
+  if (typeof to !== 'string') throw new Error("'to' param must be a string")
 }
 
 describe('reconfigString', () => {
-  it('Should be a function', () => { // 1st test
-    expect(reconfigString).toBeTypeOf('function')
-  })
+  // it('Should be a function', () => { // 1st test -> REDUNDANT
+  //   expect(reconfigString).toBeTypeOf('function')
+  // })
 
   it('Should throw if first param is missing', () => { // 2nd test
     expect(() => reconfigString()).toThrow()
   })
 
-  it('Should throw if first param is not a string', () => { // 2nd test
+  it('Should throw if first param is not a string', () => { // 3rd test
     expect(() => reconfigString(2)).toThrow()
+  })
+
+  it('Should throw if second param is not a string', () => { // 4th test
+    expect(() => reconfigString('a')).toThrow()
   })
 })
