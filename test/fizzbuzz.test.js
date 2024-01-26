0b3887f64cbaeb735db9cbe15837cd350522506a
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-26 11:29:12 +0100

    declared a function to pass the test

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,5 +1,9 @@
 import { describe, expect, it } from 'vitest'
 
+const fizzbuzz = () => {
+
+}
+
 describe('fizzbuzz', () => {
   it('Should be a function', () => {
     expect(typeof fizzbuzz).toBe('function')
