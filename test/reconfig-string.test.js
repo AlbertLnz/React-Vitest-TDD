98c56d76bb511f1a9f18ee970ddf233d516b52fa
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-26 12:43:23 +0100

    2nd example file - Reconfig-string test

---
+++ test/reconfig-string.test.js
@@ -0,0 +1,11 @@
+import { describe, expect, it } from 'vitest'
+
+const reconfigString = () => {
+  
+}
+
+describe('reconfigString', () => {
+  it('Should be a function', () => {
+    expect(reconfigString).toBeTypeOf('function')
+  })
+})
