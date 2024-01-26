f35746023f57963735b8df57f07d0681563576fe
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-26 12:51:02 +0100

    Test n3 - Throw if param is not a string

--- test/reconfig-string.test.js
+++ test/reconfig-string.test.js
@@ -2,6 +2,7 @@ import { describe, expect, it } from 'vitest'
 
 const reconfigString = (from) => {
   if (from === undefined) throw new Error("'from' param is required")
+  if (typeof from !== 'string') throw new Error("'from' param must be a string")
 }
 
 describe('reconfigString', () => {
@@ -11,5 +12,9 @@ describe('reconfigString', () => {
 
   it('Should throw if first param is missing', () => { // 2nd test
     expect(() => reconfigString()).toThrow()
+  })
+
+  it('Should throw if first param is not a string', () => { // 2nd test
+    expect(() => reconfigString(2)).toThrow()
   })
 })
