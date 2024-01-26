c92b58585788e0389422147b8f589454d4a33926
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-26 11:33:51 +0100

    solve 2nd test, fizzbuzz function pass a number param

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,15 +1,15 @@
 import { describe, expect, it } from 'vitest'
 
-const fizzbuzz = () => {
-
+const fizzbuzz = (number) => {
+  if (typeof number !== 'number') throw new Error()
 }
 
 describe('fizzbuzz', () => {
-  it('Should be a function', () => {
+  it('Should be a function', () => { // test nº1
     expect(typeof fizzbuzz).toBe('function')
   })
 
-  it('Should throw if not number is provided as parameter', () => {
+  it('Should throw if not number is provided as parameter', () => { // test nº2
     expect(() => fizzbuzz()).toThrow()
   })
 })
